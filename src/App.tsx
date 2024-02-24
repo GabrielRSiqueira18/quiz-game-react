905268a5cbf128c9424840f166653ea1b0dbf038
GabrielRSiqueira18 <gabrielrsiqueira2023@gmail.com>
2024-02-24 12:43:53 -0300

    feat: create the QuizBox component and thim styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import styles from "./app.module.css"
 import { Aside } from "./components/Aside"
 import { Header } from "./components/Header"
+import { QuizBox } from "./components/QuizBox"
 
 export function App() {
   const [ asideIsOpen, setAsideIsOpen ] = useState(false)
@@ -19,7 +20,10 @@ export function App() {
 
     <div className={styles.container}>
       <Header /> 
-      
+      <QuizBox 
+        title="s"
+        description="s"
+      />
     </div>
    </>
   )
--- src/components/QuizBox/index.tsx
+++ src/components/QuizBox/index.tsx
@@ -1,7 +1,18 @@
-export function QuizBox() {
+import styles from "./styles.module.css";
+
+interface QuizBoxProps {
+  title: string
+  description: string
+}
+
+export function QuizBox({ title, description }: QuizBoxProps) {
   return (
-    <>
-      
-    </>
+    <div className={styles['container-quiz-box']}>
+      <h2>Contas Matemáticas</h2>
+      <p>
+        Neste quiz você irá aprender melhor a usar contas básicas de matemática.
+      </p>
+      <button>Acessar</button>
+    </div>
   )
 }
