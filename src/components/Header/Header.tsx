452f2f0b2bc3012691964f592e2c302e8de9fbf9
GabrielRSiqueira18 <gabrielrsiqueira2023@gmail.com>
2024-02-24 06:26:13 -0300

    first commit. Structure the application and aply base config css [reset]

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+export function Header() {
+  return (
+    <header>
+      <h2>Quiz</h2>
+      <nav>
+        <ul>
+          <li>Rewards</li>
+          <li>Resources</li>
+        </ul>
+      </nav>
+      <div>
+        <button>Log in</button>
+        <button>Join now</button>
+      </div>
+    </header>
+  )
+}
