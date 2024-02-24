f38d8d2f546b39b175aacfbf7bc880998791c054
GabrielRSiqueira18 <gabrielrsiqueira2023@gmail.com>
2024-02-24 06:43:42 -0300

    styles: create the header styles and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import styles from "./app.module.css"
+import { Header } from "./components/Header/Header"
 
 export function App() {
   return (
     <div className={styles.container}>
-      
+      <Header /> 
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
+import styles from "./styles.module.css"
+
 export function Header() {
   return (
     <header>
-      <h2>Quiz</h2>
+      <h2>
+        <strong>Quiz </strong>
+        Game
+      </h2>
       <nav>
         <ul>
           <li>Rewards</li>
           <li>Resources</li>
         </ul>
       </nav>
-      <div>
+      <div className={styles['container-buttons']}>
         <button>Log in</button>
         <button>Join now</button>
       </div>
